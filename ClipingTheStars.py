65582ab1111faf839318295c7054c7c4a8405a50
Ullas MP <ullasmpu@gmail.com>
2026-03-27 22:04:32 +0530

    Solved more ClipingStar Patterns

--- ClipingTheStars.py
+++ ClipingTheStars.py
@@ -1,9 +1,18 @@
+print("Pattern 1")
 for i in range(1,5+1):
     for j in range(1,i+1):
         print("*",end="")
     print()
 print()
 
+print("Pattern 2")
+for i in range(1,5+1):
+    for j in range(1,i+i-1+1):
+        print("*",end="")
+    print()
+print()
+
+print("Pattern 3")
 n=5
 for i in range(1,5+1):
     for i in range(1,n-i+1+1):
@@ -11,10 +20,41 @@ for i in range(1,5+1):
     print()
 print()
 
+
+print("Pattern 4")
 n=5
 for i in range(1,5+1):
     for i in range(1,n-i+n-i+1+1):
         print("*",end="")
+    print()
+print()
+
+print("Same patern Diffrent Method")
+print()
+
+print("Pattern 5")
+for i in range(1,5+1):
+    print("*"*i, end="")
+    print()
+print()
+
+print("Pattern 6")
+for i in range(1,5+1):
+    print("*"*(i+i-1),end="")
+    print()
+print()
+
+print("Pattern 7")
+n=5
+for i in range(1,5+1):
+    print("*"*(n-i+1), end="")
+    print()
+print()
+
+print("Pattern 8")
+n=10
+for i in range(1,10+1,2):
+    print("*"*(n-i), end="")
     print()
 print()
 
