5c971cf1227b9fc7ccb63ac7491da2646b3e52f7
Ullas MP <ullasmpu@gmail.com>
2026-03-29 21:43:26 +0530

    Practiced Some more Paterns

--- BoundryPaterns.py
+++ BoundryPaterns.py
@@ -119,3 +119,15 @@ for i in range(1,n+1):
             print(" ",end=" ")
     print()
 print()
+
+print("pattern 10")
+print()
+
+n=4
+for i in range(1,4+1):
+    for j in range(1,5+1):
+        if i==1 or i==4 or j==1 or j==5:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
--- Re-Combination Patterns.py
+++ Re-Combination Patterns.py
@@ -1,3 +1,5 @@
+print("Patern 1")
+print()
 n=5
 for i in range(1,5+1):
     for j in range(1,i-1+1):
@@ -5,3 +7,37 @@ for i in range(1,5+1):
     for j in range(1,n-i+1+1):
         print("*", end="")
     print()
+print()
+
+print("Patern 2")
+print()
+n=4
+for i in range(1,n+1):
+    for j in range(1,i-1+1):
+        print(" ",end="")
+    for j in range(1,6+1):
+        print("*",end="")
+    print()
+print()
+
+print("Patern 3")
+print()
+n=4
+for i in range(1,4+1):
+    for j in range(1,n-i+1):
+        print(" ",end="")
+    for j in range(1,4+1):
+        print("*",end="")
+    print()
+print()
+
+print("Pattern 4")
+print()
+n=5
+for i in range(1,n):
+    print(" "*(n-i-1) + "*"*(i+(i-1)))
+
+
+
+
+
